c46c395f0e4044a52bb5491ce272cf42a6409c0a
cynthiawambua <cynthiamumbuawambua@gmail.com>
2023-08-19 11:45:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import Page from './page';
+import Categories from './categories';
+import Images from './images';
+import Menu from './menu';
+import Footer from './footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Page/>
+      <Categories/>
+      <Images/>
+      <Menu/>
+      <Footer/>
+   
     </div>
   );
 }
